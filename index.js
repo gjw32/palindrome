815ada65aa781e49eebcaa1ba8c87e8024a03101
Greg Williams <greg.williams.1978@gmail.com>
2018-10-06 20:50:19 -0400

    Initialize repository

---
+++ index.js
@@ -0,0 +1,27 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  this.processor = function(string) {
+    return string.toLowerCase();
+  }
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.processor(this.content);
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
+  this.palindrome = function palindrome() {
+    return this.processedContent() === reverse(this.processedContent());
+  }
+
+  // Returns the phrase in all caps
+  this.louder = function louder() { return this.content.toUpperCase(); }
+}
+
